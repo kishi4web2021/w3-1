6efec68c28a8d71ee71f7c26fc6784803de32260
kishi4web2021 <95006357+kishi4web2021@users.noreply.github.com>
2021-12-02 11:43:06 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function　() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+    myImage.setAttribute ('src','images/myimage.png');
   } else {
     myImage.setAttribute ('src','images/firefox-icon.png');
   }
